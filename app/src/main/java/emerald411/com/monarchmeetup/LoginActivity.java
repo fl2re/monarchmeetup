3b316cedc6016a689f6cd1980212beda92b2d804
AlexD <adohrn@cs.odu.edu>
2016-11-09 20:55:42 -0500

    Created details activity.

---
+++ app/src/main/java/emerald411/com/monarchmeetup/LoginActivity.java
@@ -0,0 +1,37 @@
+package emerald411.com.monarchmeetup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.tvLoginBtn)
+    TextView tvLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        tvLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Create method to check for correct login information
+
+                Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        });
+    }
+
+}
